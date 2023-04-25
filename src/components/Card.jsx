a441a1011f1bcbd27eaa76d2af23c5ead59d84db
melinamontelongo <110259601+melinamontelongo@users.noreply.github.com>
2023-04-25 13:23:53 -0300

    style: hover styling on card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 export const Card = ({ image, title, body = "", footer="", clickHandler = undefined, id = undefined }) => {
     return (
-        <div className="card bg-primary/50 backdrop-blur drop-shadow-xl md:w-96 shadow-xl m-5" onClick={clickHandler}>
+        <div className="card bg-primary/50 backdrop-blur drop-shadow-xl md:w-96 shadow-xl m-5 hover:cursor-pointer hover:shadow-lg hover:drop-shadow-lg" onClick={clickHandler}>
             <figure className="px-5 pt-5"><img className="bg-zinc-200 rounded-xl" src={image} alt={title} /></figure>
             <div className="card-body">
                 <div className="flex justify-center h-full">
